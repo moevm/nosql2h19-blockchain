5b25a175b80b42664bb694d4180dfed803f7464c
Elena Cherkasova <Elena.Cherkasova@oktetlabs.ru>
2019-11-07 12:22:26 +0300

    binding frontend scripts (update)

--- crypto_wallet/crypto_wallet_server/settings.py
+++ crypto_wallet/crypto_wallet_server/settings.py
@@ -123,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'crypto_wallet_server/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'crypto_wallet_server/static'),
+]
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
