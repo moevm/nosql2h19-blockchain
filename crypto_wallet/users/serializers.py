594cdee2774be285649bf68bb1cc675e5da889be
Elena Cherkasova <Elena.Cherkasova@oktetlabs.ru>
2019-11-11 02:10:56 +0300

    fix password for users

--- crypto_wallet/users/serializers.py
+++ crypto_wallet/users/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=64, required=True)
     email = serializers.CharField(max_length=64, required=True)
-    password = serializers.HiddenField(required=True)
+    password = serializers.CharField(write_only=True, required=True)
     registration_date = serializers.DateTimeField(required=True)
     permission = serializers.CharField(max_length=64, required=True)
 
