9160439b9bf9b1c3ed70778b7b0a1cb99b919846
Elena Cherkasova <Elena.Cherkasova@oktetlabs.ru>
2019-11-11 02:24:53 +0300

    binding with frontend

--- crypto_wallet/crypto_wallet_server/urls.py
+++ crypto_wallet/crypto_wallet_server/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-# from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='base.html'), name='base'),
     path('api/v0/jwt_auth/', include('jwt_auth.urls')),
     path('api/v0/users/', include('users.urls')),
     path('api/v0/wallets/', include('wallets.urls')),
