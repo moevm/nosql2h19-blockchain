7bf2ec3843da39f8dedbd9fbd1ef21c19766bfcc
DimitryS <dimsf97@mail.ru>
2019-11-10 17:01:16 +0300

    Add client

--- crypto_wallet/crypto_wallet_server/urls.py
+++ crypto_wallet/crypto_wallet_server/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='base.html'), name='home'),
+    path('authorization/', TemplateView.as_view(template_name='base.html'), name='home'),
     path('crypro_wallet/', include('crypto_wallet_app.urls')),
 ]
