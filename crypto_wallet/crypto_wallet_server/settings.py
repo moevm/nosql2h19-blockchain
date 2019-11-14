a22e5c386e14c77620d38cfa3b66bff00fdc1680
Elena Cherkasova <Elena.Cherkasova@oktetlabs.ru>
2019-11-14 17:02:57 +0300

    mode base.html to /client/public

--- crypto_wallet/crypto_wallet_server/settings.py
+++ crypto_wallet/crypto_wallet_server/settings.py
@@ -65,7 +65,10 @@ ROOT_URLCONF = 'crypto_wallet_server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'client/public')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
